47e6d2dd62863ba91b6c24098d641baee8c75589
Jordan Bienvenue <jord.bienvenue@gmail.com>
2023-08-28 10:53:06 +0400

    update workspace + import tailwind

---
+++ packages/styles/plugin.js
@@ -0,0 +1,23 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = plugin(function ({ }) { }, {
+    theme: {
+        // fontSize: {
+        //     sm: 16,
+        //     md: 24,
+        //     lg: 32
+        // },
+        colors: {
+            yellow: {
+                300: '#F0DB4F'
+            },
+            gray: {
+                200: '#ABB2BF',
+                500: '#767C87',
+                800: '#282C33',
+            },
+            white: '#FFFFFF',
+            primary: "#FF0000"
+        },
+    }
+});
